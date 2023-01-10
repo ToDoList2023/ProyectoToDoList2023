27b056a94b1938b261113c9e166a4d88683faf17
Elvia Benedith <ejbenedith@gmail.com>
2023-01-11 00:03:12 +0100

    servicios testeados y expertados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  
   return (
     <>
-    TO DO LIST
+      HOLA MUNDO
     </>
   );
 }
--- src/services/serviceAxios.js
+++ src/services/serviceAxios.js
@@ -2,17 +2,38 @@ import axios from 'axios';
 
 
 const mostrarTareas = async () => {
-    const peticion = await axios.get('http://localhost:3000/tareas')
-    console.log(peticion.tarea);
-    
+    const respuesta = await axios.get('http://localhost:5000/tareas')
+    return respuesta.data;
+}
+
+const agregarNuevaTarea = async (tarea) => {
+    const respuesta = await axios.post('http://localhost:5000/tareas', { tarea: tarea, completado: false});
+    return respuesta.data.json;
+}
+
+const editarTarea = async ({id, tarea, completado}) => {
+    const peticion = {};
+    if (tarea){
+        peticion.tarea = tarea;
+    }
+    if (completado !== undefined){
+        peticion.completado = completado;
+    }
+   
+    const respuesta = await axios.patch(`http://localhost:5000/tareas/${id}`, {tarea: tarea, completado: completado});
+    return respuesta;
+}
+
+const eliminarTarea = async(id) => {
+    const respuesta = await axios.delete(`http://localhost:5000/tareas/${id}`);
+    return respuesta;
 }
 
 
 
-
-
-
-
 export {
-    mostrarTareas 
+    mostrarTareas,
+    agregarNuevaTarea, 
+    editarTarea,
+    eliminarTarea
 }
